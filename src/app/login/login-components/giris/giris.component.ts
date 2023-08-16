c64b887d663dfb37de578f4183d3ba16f0921268
Ali Arslan Ateş <aliates7118@gmail.com>
2023-08-16 17:58:15 +0300

    giriş butonu aktifleştirildi

--- src/app/login/login-components/giris/giris.component.ts
+++ src/app/login/login-components/giris/giris.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-giris',
   templateUrl: './giris.component.html',
   styleUrls: ['./giris.component.scss']
 })
-export class GirisComponent {
- t:string  = "Hello World";
+export class GirisComponent implements OnInit {
+
+  isLoading = false;
+  constructor(private router: Router) { }
+
+  ngOnInit(): void
+  {
+
+  }
+  goToPersonel(){
+    this.router.navigateByUrl('/personel-home');
+  }
+
+  login = () => {
+    this.isLoading = true;
+
+  setTimeout(() =>  {
+      this.isLoading = false;
+    }, 3000);
+
+  }
+
 }
 
 
