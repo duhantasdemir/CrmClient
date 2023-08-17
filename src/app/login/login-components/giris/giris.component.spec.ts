04edb3a0f17eacb36b5a5b2ce7c231ea4fb5a2bf
Ali Arslan Ateş <aliates7118@gmail.com>
2023-08-17 16:02:25 +0300

    17.08.2023 16:02

--- src/app/login/login-components/giris/giris.component.spec.ts
+++ src/app/login/login-components/giris/giris.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { GirisComponent } from './giris.component';
 
 describe('GirisComponent', () => {
--- src/app/login/login-components/giris/giris.component.ts
+++ src/app/login/login-components/giris/giris.component.ts
@@ -1,30 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { girisService } from './giris.service';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { login } from './giris';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
 @Component({
   selector: 'app-giris',
   templateUrl: './giris.component.html',
   styleUrls: ['./giris.component.scss']
 })
 export class GirisComponent implements OnInit {
-  model: any= {};
+  /* model: any= {};
   Email! : string ;
   password! :string ;
+  username! : string; */
+  profileForm: FormGroup;
+  isLoading: boolean = false;
 
-  constructor(private router: Router, 
-    private girisService: girisService,
-    public service: girisService, 
-    private fb: FormBuilder) { }
-
-  profileForm = this.fb.group({
-    username: [''],
-    password: [''],
-  });
-
-  isLoading = false;
-  getData!: boolean;
+  constructor(private formBuilder: FormBuilder) {
+    this.profileForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
   
 
   ngOnInit(): void
@@ -34,17 +30,20 @@ export class GirisComponent implements OnInit {
   //    this.router.navigateByUrl('/personel-home');
   //  }
 
+
+  
   login()  {
-    console.log(this.Email);
-    console.log(this.password);
+    console.log(this.login)
+    if (this.profileForm.valid) {
+    this.isLoading = true;
   //   this.girisService.postEkle(this.model.Email, this.model.password).subscribe((res : any)=>{
   //   this.getData = res; //0 or 1
 
   //    })
-  //  setTimeout(() =>  {
-  //      this.isLoading = false;
-  //    }, 3000);
+    setTimeout(() =>  {
+        this.isLoading = false;
+      }, 3000);
 
   }
 
-}
+  }}
