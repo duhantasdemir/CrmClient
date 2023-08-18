d150c7ecef3db44597ea770ed3f2c88ae2f36dde
duhantasdemir <emirtasdemir19@gmail.com>
2023-08-18 12:21:19 +0300

    login eklemeleri yapıldı

--- src/app/login/login-components/giris/giris.component.ts
+++ src/app/login/login-components/giris/giris.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GirisService } from './giris.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-giris',
@@ -7,18 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class GirisComponent implements OnInit {
-
-  constructor() { }
+  constructor(public girisService: GirisService) { }
 
   ngOnInit(): void {
     
   }
 
-  login(email: string, password: string) {
-    console.log("E mail" , email);
-    console.log("Pass" , password);
+
+
+  login(arg0: string,arg1: string){  
+    // if(form.invalid){  
+    //   return;  
+    // }  
+    console.log("form başarılı");
+    this.girisService.CreateUser(arg0, arg1);  
+  }  
+
   }
     
-}
+
 
 
--- src/app/login/login-components/giris/giris.ts
+++ src/app/login/login-components/giris/giris.ts
@@ -1,4 +1,4 @@
 export class login {
-  Email!: string;
+  email!: string;
   password!: string;
 }
